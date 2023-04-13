2ef2298395b4074f8c793acb3679005206dfab33
ToluOjo <ojotolulope137@gmail.com>
2023-04-13 01:45:23 +0100

    register user with argon2 password hashing

--- dist/controller/userControler.js
+++ dist/controller/userControler.js
@@ -14,15 +14,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.register = void 0;
 const userModel_1 = __importDefault(require("../model/userModel"));
+const argon2_1 = __importDefault(require("argon2"));
 const register = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { email, password, confirmPassword } = req.body;
+        const hashedPassword = yield argon2_1.default.hash(password, { hashLength: 10 });
         const oldUser = yield userModel_1.default.findOne({ email });
         if (oldUser) {
             return res.status(400).json({ message: "email already exist" });
         }
+        if (yield argon2_1.default.verify(hashedPassword, confirmPassword)) {
+            return res.status(400).json({ message: "password and confirmPassword does not match" });
+        }
         const newUser = new userModel_1.default({
-            email, password
+            email,
+            password: hashedPassword,
         });
         yield newUser.save();
         return res.status(201).json({ message: "You are Sucessfully registered" });
--- src/controller/userControler.ts
+++ src/controller/userControler.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from "express";
 import User from "../model/userModel";
+import argon2 from "argon2"
 
 export const register = async (req: Request, res: Response) => {
   try {
     const { email, password, confirmPassword } = req.body;
+    
+    const hashedPassword =await argon2.hash(password,{hashLength: 10})
 
     const oldUser = await User.findOne({ email });
     if (oldUser) {
       return res.status(400).json({ message: "email already exist" });
     }
+    if (await argon2.verify(hashedPassword,confirmPassword)){
+        return res.status(400).json({message: "password and confirmPassword does not match"})
+    }
     const newUser=new User({
-        email,password
+        email,
+        password:hashedPassword,
     })
     await newUser.save()
     
@@ -20,3 +27,4 @@ export const register = async (req: Request, res: Response) => {
     console.log(error);
   }
 }
+
